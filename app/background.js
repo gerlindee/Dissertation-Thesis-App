1c2fc07c45e521cf01c5f09578256ef1fa22cee3
Gerlinde Christine Ciocea <ciocea.gerlinde@gmail.com>
2022-05-04 18:04:19 +0300

    #1: Small refactoring
    Modularisation of repeated code in background script

--- app/background.js
+++ app/background.js
@@ -7,10 +7,6 @@ const settings = {
     defaultPosTone: "#A7C957",
     defaultNegTone: "#BC4749",
     defaultNeutralTone: "#F2E8CF",
-
-    defaultAccPos: "#F7B801",
-    defaultAccNeg: "#7678ed",
-    defaultAccNeutral: "#F2E8CF",
 }
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -29,28 +25,38 @@ chrome.runtime.onInstalled.addListener(function () {
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     if (info.menuItemId === "text_select") {
-        console.log(info.frameId)
+        if (settings.positiveToneColour === "#FFFFFF" &&
+            settings.negativeToneColour === "#FFFFFF" &&
+            settings.neutralToneColour === "#FFFFFF") {
 
-        chrome.storage.sync.get(["positiveToneColour"], (res) => {
-            if (res.positiveToneColour !== undefined) {
-                settings.positiveToneColour = res.positiveToneColour
-            } else {
-                settings.positiveToneColour = settings.defaultPosTone
-            }
+            chrome.storage.sync.get(["positiveToneColour"], (res) => {
+                if (res.positiveToneColour !== undefined) {
+                    settings.positiveToneColour = res.positiveToneColour
+                } else {
+                    settings.positiveToneColour = settings.defaultPosTone
+                }
 
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                files: ['content.js'],
-            }, function () {
-                chrome.tabs.sendMessage(tab.id, {
-                    action: "highlight_selection",
-                    colour: settings.positiveToneColour
-                })
+                callHighlightingScript(tab, settings.positiveToneColour)
             })
-        })
+        } else {
+            callHighlightingScript(tab, settings.negativeToneColour)
+        }
+
     }
 
     if (info.menuItemId === "image_select") {
         console.log(info.srcUrl)
     }
 })
+
+function callHighlightingScript(tab, colour) {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: ['content.js'],
+    }, function () {
+        chrome.tabs.sendMessage(tab.id, {
+            action: "highlight_selection",
+            colour: colour
+        })
+    })
+}
