c8e5b721ade10b9567811dea711351b277f36cc6
Gerlinde Christine Ciocea <ciocea.gerlinde@gmail.com>
2022-05-06 20:24:36 +0300

    #8: Implement accessibility mode
    User can toggle between colour blind friendly and regular in the pop-up; this now also changes the entire pop-up colour scheme, not just the Pos/Neg/Gen highlight colours :+1:

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,143 +1,307 @@
 // Global Variables declaration
-let isPermanentModeEnabled = false
+let isAccessibilityModeEnabled = false;
+
 let toneColours = {
     positiveTone: "#FFFFFF",
     negativeTone: "#FFFFFF",
-    neutralTone:  "#FFFFFF"
-}
+    neutralTone: "#FFFFFF",
+};
+
 const defaults = {
     positiveTone: "#A7C957",
     negativeTone: "#BC4749",
-    neutralTone:  "#F2E8CF",
+    neutralTone: "#F2E8CF",
 
     accPositiveTone: "#F7B801",
     accNegativeTone: "#7678ED",
-    accNeutralTone:  "#F2E8CF"
-}
+    accNeutralTone: "#F2E8CF"
+};
+
+const themeColours = {
+    background: "#EBC2C1",
+    primary: "#CD5D51",
+    accent: "#904139",
+
+    background_acc: "#d6d7fa",
+    primary_acc: "#7678ed",
+    accent_acc: "#5e60be"
+};
 
 window.onload = function () {
-    initialiseEventHandlers()
-    initialiseSettings()
-}
+    createEventHandlers()
+    loadSettings()
+};
 
-function initialiseEventHandlers() {
-    document
-        .getElementById("check_permanent_mode")
-        .addEventListener("click", (event) => {
-            chrome.storage.sync.set({isPermanentModeEnabled: event.target.checked})
-         })
-
+/**
+ * @name createEventHandlers
+ * @description Add event handlers for the two buttons in the popup, as well as for the three colour picker inputs
+ */
+function createEventHandlers() {
+    // Event handler for when the positive tone colour is changed
     document
         .getElementById("colour_theme_pos")
         .addEventListener("change", (event) => {
-            chrome.storage.sync.set({ positiveToneColour: event.target.value })
-        })
+            cachePositiveToneColour(event.target.value);
+        });
 
+    // Event handler for when the negative tone colour is changed
     document
         .getElementById("colour_theme_neg")
         .addEventListener("change", (event) => {
-            chrome.storage.sync.set({ negativeToneColour: event.target.value })
-        })
+            cacheNegativeToneColour(event.target.value);
+        });
 
+    // Event handler for when the neutral tone colour is changed
     document
         .getElementById("colour_theme_gen")
         .addEventListener("change", (event) => {
-            chrome.storage.sync.set({ neutralToneColour: event.target.value })
-        })
+            cacheNeutralToneColour(event.target.value);
+        });
 
     document
         .getElementById("reset_colours")
         .addEventListener("click", () => {
-            handleColourReset(false)
-        })
+           setDefaultColours(isAccessibilityModeEnabled)
+        });
 
+    // Event handler for when accessibility mode is enabled or disabled
     document
-        .getElementById("switch_acc_colours")
+        .getElementById("set_accessibility_colours")
         .addEventListener("click", () => {
-            handleColourReset(true)
-        })
+            chrome.storage.sync.set({isAccessibilityModeEnabled: !(isAccessibilityModeEnabled)})
+                .then(() => {
+                    isAccessibilityModeEnabled = !(isAccessibilityModeEnabled);
+                    setDefaultColours(isAccessibilityModeEnabled);
+                    setColourTheme(isAccessibilityModeEnabled);
+                });
+        });
 }
 
-function initialiseSettings() {
-    initialisePermanentMode()
-    initialisePositiveToneColour()
-    initialiseNegativeToneColour()
-    initialiseNeutralToneColour()
-}
-
-function initialisePermanentMode() {
-    chrome.storage.sync.get(["isPermanentModeEnabled"], (res) => {
-        handlePermanentModeChangeEvent(res.isPermanentModeEnabled)
+/**
+ * @name getSettings
+ * @description Load the settings from the local cache of the extension into the popup screen
+ */
+function loadSettings() {
+    chrome.storage.sync.get(["isAccessibilityModeEnabled"], (res) => {
+        if (res.isAccessibilityModeEnabled === undefined) {
+            isAccessibilityModeEnabled = false;
+        } else {
+            isAccessibilityModeEnabled = res.isAccessibilityModeEnabled;
+        }
+        setColourTheme(isAccessibilityModeEnabled);
+        loadPositiveToneColour();
+        loadNegativeToneColour();
+        loadNeutralToneColour();
     })
 }
 
-function initialisePositiveToneColour() {
+/**
+ * @name setPositiveToneColour
+ * @description Set the hex code for the Positive Tone Colour
+ */
+function setPositiveToneColour(value) {
+    toneColours.positiveTone = value;
+    document.getElementById("colour_theme_pos").value = value;
+}
+
+/**
+ * @name loadPositiveToneColour
+ * @description Load the positive tone colour into the colour picker; If no value is stored, set the default value
+ */
+function loadPositiveToneColour() {
     chrome.storage.sync.get(["positiveToneColour"], (res) => {
         if (res.positiveToneColour !== undefined) {
-            handlePositiveToneColourChange(res.positiveToneColour)
+            setPositiveToneColour(res.positiveToneColour);
         } else {
-            handlePositiveToneColourChange(defaults.positiveTone)
+            if (isAccessibilityModeEnabled === true) {
+                setPositiveToneColour(defaults.accPositiveTone);
+            } else {
+                setPositiveToneColour(defaults.positiveTone);
+            }
         }
     })
 }
 
-function initialiseNegativeToneColour() {
+/**
+ * @name cachePositiveToneColour
+ * @description Store the selected positive tone colour into the cache
+ * @param {string} colour
+ */
+function cachePositiveToneColour(colour) {
+    chrome.storage.sync.set({positiveToneColour: colour})
+        .then(() => {
+            setPositiveToneColour(colour);
+        })
+}
+
+/**
+ * @name setNegativeToneColour
+ * @description Set the hex code for the negative tone colour
+ */
+function setNegativeToneColour(value) {
+    toneColours.negativeTone = value;
+    document.getElementById("colour_theme_neg").value = value;
+}
+
+/**
+ * @name loadNegativeToneColour
+ * @description Load the negative tone colour into the colour picker; If no value is stored, set the default value
+ */
+function loadNegativeToneColour() {
     chrome.storage.sync.get(["negativeToneColour"], (res) => {
         if (res.negativeToneColour !== undefined) {
-            handleNegativeToneColourChange(res.negativeToneColour)
+            setNegativeToneColour(res.negativeToneColour);
         } else {
-            handleNegativeToneColourChange(defaults.negativeTone) // default negative colour
+            if (isAccessibilityModeEnabled === true) {
+                setNegativeToneColour(defaults.accNegativeTone);
+            } else {
+                setNegativeToneColour(defaults.negativeTone);
+            }
         }
-    })
+    });
 }
 
-function initialiseNeutralToneColour() {
+/**
+ * @name cacheNegativeToneColour
+ * @description Store the selected negative tone colour into the cache
+ * @param {string} colour
+ */
+function cacheNegativeToneColour(colour) {
+    chrome.storage.sync.set({negativeToneColour: colour})
+        .then(() => {
+            setNegativeToneColour(colour);
+        })
+}
+
+/**
+ * @name setNeutralToneColour
+ * @description Set the hex code for the neutral tone colour
+ */
+function setNeutralToneColour(value) {
+    toneColours.neutralTone = value;
+    document.getElementById("colour_theme_gen").value = value;
+}
+
+/**
+ * @name loadNeutralToneColour
+ * @description Load the neutral tone colour into the colour picker; If no value is stored, set the default value
+ */
+function loadNeutralToneColour() {
     chrome.storage.sync.get(["neutralToneColour"], (res) => {
         if (res.neutralToneColour !== undefined) {
-            handleNeutralToneColourChange(res.neutralToneColour)
+            setNeutralToneColour(res.neutralToneColour);
         } else {
-            handleNeutralToneColourChange(defaults.neutralTone)
+            if (isAccessibilityModeEnabled === true) {
+                setNeutralToneColour(defaults.accNeutralTone);
+            } else {
+                setNeutralToneColour(defaults.neutralTone);
+            }
         }
-    })
+    });
 }
 
-function handlePermanentModeChangeEvent(value) {
-    isPermanentModeEnabled = value;
-    document.getElementById("check_permanent_mode").checked = value;
+/**
+ * @name cacheNeutralToneColour
+ * @description Store the selected neutral tone colour into the cache
+ * @param {string} colour
+ */
+function cacheNeutralToneColour(colour) {
+    chrome.storage.sync.set({neutralToneColour: colour})
+        .then(() => {
+            setNeutralToneColour(colour);
+        })
 }
 
-function handlePositiveToneColourChange(value) {
-    toneColours.positiveTone = value
-    document.getElementById("colour_theme_pos").value = value
-}
-
-function handleNegativeToneColourChange(value) {
-    toneColours.negativeTone = value
-    document.getElementById("colour_theme_neg").value = value
-}
-
-function handleNeutralToneColourChange(value) {
-    toneColours.neutralTone = value
-    document.getElementById("colour_theme_gen").value = value
-}
-
-function handleColourReset(accMode) {
+/**
+ * @name setDefaultColours
+ * @description Load the default colours into the colour picker inputs
+ * @param {boolean} accMode
+ */
+function setDefaultColours(accMode) {
     if (accMode === true) {
-        toneColours.positiveTone = defaults.accPositiveTone
-        toneColours.negativeTone = defaults.accNegativeTone
-        toneColours.neutralTone  = defaults.accNeutralTone
+        cachePositiveToneColour(defaults.accPositiveTone);
+        setPositiveToneColour(defaults.accPositiveTone);
+
+        cacheNegativeToneColour(defaults.accNegativeTone);
+        setNegativeToneColour(defaults.accNegativeTone);
+
+        cacheNeutralToneColour(defaults.accNeutralTone);
+        setNeutralToneColour(defaults.accNeutralTone);
     } else {
-        toneColours.positiveTone = defaults.positiveTone
-        toneColours.negativeTone = defaults.negativeTone
-        toneColours.neutralTone  = defaults.neutralTone
+        cachePositiveToneColour(defaults.positiveTone);
+        setPositiveToneColour(defaults.positiveTone);
+
+        cacheNegativeToneColour(defaults.negativeTone);
+        setNegativeToneColour(defaults.negativeTone);
+
+        cacheNeutralToneColour(defaults.neutralTone);
+        setNeutralToneColour(defaults.neutralTone);
     }
+}
 
-    handlePositiveToneColourChange(toneColours.positiveTone)
-    chrome.storage.sync.set({ positiveToneColour: toneColours.positiveTone })
+/**
+ * @name setColourTheme
+ * @description Set the popup colour theme, based on whether accessibility mode is selected or not
+ * @param accMode
+ */
+function setColourTheme(accMode) {
+    if (accMode === true) {
+        document.getElementById("set_accessibility_colours").innerText = "Disable Colour Blind Friendly Mode";
 
-    handleNegativeToneColourChange(toneColours.negativeTone)
-    chrome.storage.sync.set({ negativeToneColour: toneColours.negativeTone })
+        document.body.style.background = themeColours.background_acc;
+        document.body.style.borderColor = themeColours.primary_acc;
+        document.getElementById("app_icon_text").src = "../images/gen_text_acc.png";
 
-    handleNeutralToneColourChange(toneColours.neutralTone)
-    chrome.storage.sync.set({ neutralToneColour: toneColours.neutralTone })
+        let elements = document.getElementsByTagName("h1");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary_acc;
+        }
+
+        elements = document.getElementsByTagName("hr");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.borderColor = themeColours.primary_acc;
+        }
+
+        elements = document.getElementsByTagName("label");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary_acc;
+        }
+
+        let hoverCSS = ".buttonHover { color: #5e60be; }";
+        let hoverStyle = document.createElement("style");
+        hoverStyle.style.cssText = hoverCSS;
+        elements = document.getElementsByClassName("colours_btn");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary_acc;
+            elements[idx].style.backgroundColor = themeColours.background_acc;
+        }
+
+    } else {
+        document.getElementById("set_accessibility_colours").innerText = "Enable Colour Blind Friendly Mode";
+
+        document.body.style.background = themeColours.background;
+        document.body.style.borderColor = themeColours.primary;
+        document.getElementById("app_icon_text").src = "../images/gen_text.png";
+
+        let elements = document.getElementsByTagName("h1");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary;
+        }
+
+        elements = document.getElementsByTagName("hr");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.borderColor = themeColours.primary;
+        }
+
+        elements = document.getElementsByTagName("label");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary;
+        }
+
+        elements = document.getElementsByClassName("colours_btn");
+        for (let idx = 0; idx < elements.length; idx++) {
+            elements[idx].style.color = themeColours.primary;
+            elements[idx].style.backgroundColor = themeColours.background;
+        }
+    }
 }
